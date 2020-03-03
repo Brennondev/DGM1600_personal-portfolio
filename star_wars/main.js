efd319d8fd6c93149b36261326ff0287a12472e9
Brennon Flitton <brennonflitton@brennons-mbp.ad.uvu.edu>
2020-03-03 12:42:38 -0700

    Assigned pictures to persons.

--- star_wars/main.js
+++ star_wars/main.js
@@ -2,50 +2,73 @@ import { films } from "../data/films.js";
 import { people } from "../data/people.js";
 import { starships } from "../data/starships.js";
 
-const greetingDiv = document.querySelector(".greeting")
-const maleButton = document.querySelector('#maleButton')
-const femaleButton = document.querySelector('#femaleButton')
-const otherButton = document.querySelector('#otherButton')
-
-const maleCharacters = people.filter(person => person.gender === "male")
-console.log(maleCharacters)
-
-const femaleCharacters = people.filter(person => person.gender === "female")
-console.log(femaleCharacters)
+const greetingDiv = document.querySelector(".greeting");
+const maleButton = document.querySelector("#maleButton");
+const femaleButton = document.querySelector("#femaleButton");
+const otherButton = document.querySelector("#otherButton");
 
 const otherCharacters = people.filter(person => {
-    if (person.gender === "hermaphrodite" || person.gender === "n/a"){
-        return person
-    }
-})
-
-let counter = 1;
-
-people.forEach(person => {
-
-  let anchorWrap = document.createElement("a");
-  anchorWrap.href = "#";
-
-  let imageItem = document.createElement("img");
-  imageItem.src = `https://starwars-visualguide.com/assets/img/characters/${counter}.jpg`;
-
-  imageItem.addEventListener("error", event => {
-    //console.log(`${event.type}: Loading image\n`)
-    //console.log(event)
-    imageItem.hidden = true;
-    //imageItem.src = '../Images/uvu_logo.png'
-  });
-
-  imageItem.addEventListener("click", event => {
-    console.log(event);
-  });
-  anchorWrap.appendChild(imageItem);
-  greetingDiv.appendChild(anchorWrap);
-  counter++;
+  if (
+    person.gender === "hermaphrodite" ||
+    person.gender === "none" ||
+    person.gender === "n/a"
+  ) {
+    return person;
+  }
 });
 
-maleButton.addEventListener("click", (event) => {
-    console.log("Clicked on maleButton")
-})
+/*console.log(maleCharacters.length);
+console.log(femaleCharacters.length);
+console.log(otherCharacters.length);*/
+
+maleButton.addEventListener("click", event => {
+  populateDOM(people.filter(person => person.gender === "male"));
+});
+
+femaleButton.addEventListener("click", event => {
+  populateDOM(people.filter(person => person.gender === "female"));
+});
+
+otherButton.addEventListener("click", event => {
+  populateDOM(otherCharacters);
+});
+
+//"url": "https://swapi.co/api/people/10/"
+
+function getCharNum(url) {
+  let end = url.lastIndexOf("/");
+  let start = end - 2;
+  if (url.charAt(start) === "/") {
+    start++;
+  }
+  return url.slice(start, end);
+}
+
+//getCharNum("https://swapi.co/api/people/1/")
+
+function populateDOM(characters) {
+  characters.forEach(person => {
+    //need to extract the number from the person.url property
+    let charNum = getCharNum(person.url);
+    let anchorWrap = document.createElement("a");
+    anchorWrap.href = "#";
+
+    let imageItem = document.createElement("img");
+    imageItem.src = `https://starwars-visualguide.com/assets/img/characters/${charNum}.jpg`;
+
+    imageItem.addEventListener("error", event => {
+      //console.log(`${event.type}: Loading image\n`)
+      //console.log(event)
+      imageItem.hidden = true;
+      //imageItem.src = '../Images/uvu_logo.png'
+    });
+
+    imageItem.addEventListener("click", event => {
+      console.log(event);
+    });
+    anchorWrap.appendChild(imageItem);
+    greetingDiv.appendChild(anchorWrap);
+  });
+}
 
 greetingDiv.appendChild(castList);
