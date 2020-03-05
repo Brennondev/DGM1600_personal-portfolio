eb9394d558a015a28b9441f518d7299bb8bfb02b
Brennon Flitton <brennonflitton@gmail.com>
2020-03-05 12:46:08 -0700

    Added navbar and ordered list to Starships

--- star_wars/main.js
+++ star_wars/main.js
@@ -1,6 +1,6 @@
-import { films } from "../data/films.js";
+
 import { people } from "../data/people.js";
-import { starships } from "../data/starships.js";
+
 
 const gallery = document.querySelector(".gallery");
 const maleButton = document.querySelector("#maleButton");
