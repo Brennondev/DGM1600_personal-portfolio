5510f231ff563034748cf1059ab39a0c5483c749
Brennon Flitton <brennonflitton@gmail.com>
2020-04-07 12:36:04 -0600

    Not finished pokemon data

--- pokemon/index.js
+++ pokemon/index.js
@@ -1,16 +1,68 @@
+const allPokemon = []
+
 function getPokeData(url) {
-  let pokemonGrid = document.querySelector(".pokemonGrid");
   fetch(url).then(function(response) {
-      response.json().then(function (data) {
-        console.log(data)
-        //pokemonGrid.textContent = data.name
-      })
+    response.json().then(function(pokeData) {
+      console.log(pokeData.results);
+      const pokeMap = pokeData.results.map(pokemon => {
+        return fetch(pokemon.url).then(resData => {
+          resData.json().then(pokeJson => {
+            allPokemon.push(pokeJson)
+          });
+        });
+      });
+    });
   });
 }
 
-getPokeData("https://pokeapi.co/api/v2/pokemon/1/");
+let pokemonGrid = document.querySelector(".pokemonGrid");
 
-var card = document.querySelector('.card');
-card.addEventListener( 'click', function() {
-  card.classList.toggle('is-flipped');
-});
+getPokeData("https://pokeapi.co/api/v2/pokemon?&limit=25");
+
+console.log(allPokemon)
+
+populatePokeCards(allPokemon)
+
+function populatePokeCards(pokeArray) {
+  pokeArray.forEach(pokemon => {
+    let pokeScene = document.createElement("div");
+    pokeScene.className = "scene";
+    let pokeCard = document.createElement("div");
+    pokeScene.className = "card";
+    pokecard.addEventListener("click", () =>
+      pokecard.classList.toggle("is-flipped")
+    );
+    let pokeFront = document.createElement("div");
+    pokeScene.className = "card__face card__face--front";
+    pokeFront.textContent = pokemon.name;
+    let pokeBack = document.createElement("div");
+    pokeScene.className = "card__face card__face--back";
+    pokeBack.textContent = "back";
+
+    pokeCard.appendChild(pokeFront);
+    pokeCard.appendChild(pokeBack);
+    pokeScene.appendChild(pokeCard);
+    pokemonGrid.appendChild(pokeScene);
+  });
+}
+
+/* var card = document.querySelector(".card");
+card.addEventListener("click", function() {
+  card.classList.toggle("is-flipped");
+}) */
+
+/* <div class="scene">
+  <div class="card">
+    <div class="card__face card__face--front">
+      <figure>
+        <img src="../Images/PokemonLogo.png" />
+        <figcaption>Bulbasaur</figcaption>
+      </figure>
+    </div>
+    <div class="card__face card__face--back">
+      <div>
+        <p>Pokemon attributes</p>
+      </div>
+    </div>
+  </div>
+</div> */
